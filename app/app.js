a7cb785035a02bec366678d1e61c0c833eb9c0af
Michal Nowak <michal.nowakb@gmail.com>
2021-06-18 12:42:40 +0100

    little change in comment for app.js file to check if jenkins works

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = express();
 var exec = require('child_process').exec;
 var mongoose = require('mongoose');
 var Post = require('./models/post');
-
+// settings
 app.set('view engine' , 'ejs');
 
 app.use(express.static('public'));
